1064f01e855da3da139080f45adc7b32525e34c9
kimverity <131386009+kimverity@users.noreply.github.com>
2023-10-04 12:04:39 +0100

    Update calculate_DJ_i_a.R
    Replace for loop with mapply().

--- R/calculate_DJ_i_a.R
+++ R/calculate_DJ_i_a.R
@@ -35,60 +35,57 @@
 #' @export
 calculate_DJ_i_a <- function(tree, node, abundances, curr_ancestor, h, l_i, 
                              index_letter, q, individual) {
-  
+
   index_letter <- toupper(index_letter) # Capitalise input
-  
+
   S_i_a_res <- calculate_S_i_a(tree,node,abundances, curr_ancestor, h, l_i) # Run function
   df_S_i_a <- S_i_a_res[[1]] # Select dataframe
   abund_list <- S_i_a_res[[2]] # Select abundance list
-  
-  # Create empty list for dataframes
-  df_In_i_a_list <- vector(mode = "list", length = length(df_S_i_a$x))
-  
-  # Sum over every region of x
-  for (k in 1:length(abund_list)){
-    S <- df_S_i_a[k,"S_i"] # Select S_i_a
-    # Select abundances in current region
-    abund_vec <- unlist(abund_list[k], use.names = FALSE)
-    
-    if (individual == FALSE){ # Calculating all indices
-      D0 <- log(length(abund_vec))
-      D1 <- sum(-(abund_vec / S) * log((abund_vec / S)))
-      # J is defined to be 1 when only one branch is present
-      if (length(abund_vec) > 1 ) { # There is more than one branch in region
-        J1 <- sum(-(abund_vec / S) * log((abund_vec / S), base=length(abund_vec)))
-      }else if (length(abund_vec) == 1) { # There is one branch in region
-        J1 <- 1
-      }
-      # Store values and corresponding x
-      df_In_i_a_list[k] <- list(data.frame("D0" = D0, "D1" = D1, "J1" = J1,
-                                           "x" = df_S_i_a[k, "x"]))
-    }else if (individual == TRUE){ # Calculating one index
+
+  # Function to calculate the index/indices sum/s
+  sum_function <- function(abund_vec, S, individual, index_letter){
+    if (individual == TRUE) { # Calculating one index
       # Checks desired index and calculates value
-      if ((index_letter == "D")&(q == 0)) {
-        ind_val <- log(length(abund_vec))
-      }else if ((index_letter == "D")&(q == 1)) {
-        ind_val <- sum(-(abund_vec / S) * log((abund_vec / S)))
-      }else if (index_letter == "J") {
-        if (length(abund_vec) > 1 ) { # There is more than one branch in region
-          ind_val <- sum(-(abund_vec / S) * log((abund_vec / S), base=length(abund_vec)))
+      if (index_letter == "J") { # J
+        # J is defined to be 1 when only one branch is present
+        if (length(abund_vec) != 1) { # There is more than one branch in region
+          ind_val <- sum(-(abund_vec / S) * log((abund_vec / S),
+                                                base=length(abund_vec)))
         }else if (length(abund_vec) == 1) { # There is one branch in region
           ind_val <- 1
         }
+      }else { # D
+        # Checks q and calculates corresponding index
+        if (q == 1) {
+          ind_val <- sum(-(abund_vec / S) * log((abund_vec / S)))
+        }else if (q == 0) {
+          ind_val <- log(length(abund_vec))
+        }
       }
-      # Store value and corresponding x
-      df_In_i_a_list[k] <- list(data.frame("In_i_a" = ind_val, "x" = df_S_i_a[k, "x"]))
+      return(ind_val)
+    }else if (individual == FALSE) { # Calculate all indices
+      D1 <- sum(-(abund_vec / S) * log((abund_vec / S)))
+      D0 <- log(length(abund_vec))
+      if (length(abund_vec) != 1) { # There is more than one branch in region
+        J1 <- sum(-(abund_vec / S) * log((abund_vec / S),
+                                         base=length(abund_vec)))
+      }else if (length(abund_vec) == 1) { # There is one branch in region
+        J1 <- 1
+      }
+      return(c(D0, D1, J1))
     }
   }
-  # Return either a dictionary of index values or single index
-  if (individual == FALSE) {
-    # Combine dataframes
-    df_In_i_a <- Reduce(rbind, df_In_i_a_list)
-    return(list("1DN" = df_In_i_a[,c("D1", "x")], "1JN" = df_In_i_a[,c("J1", "x")],
-                "0DN" = df_In_i_a[,c("D0", "x")]))
-  }else if (individual == TRUE){
-    # Combine dataframes
-    df_In_i_a <- Reduce(rbind, df_In_i_a_list)
-    return(df_In_i_a)
+  # Calculate index values
+  values <- mapply(sum_function, abund_vec = abund_list, S = df_S_i_a$S_i, 
+                   individual=individual, index_letter=index_letter)
+
+  # Return either a dictionary of dataframes with index values or 
+  # a single dataframe with desired index values
+  if (individual == FALSE) { # All indices
+    return(list("1DN" = data.frame("D1" = values[2,], "x" = df_S_i_a$x),
+                "1JN" = data.frame("J1" = values[3,], "x" = df_S_i_a$x),
+                "0DN" = data.frame("D0" = values[1,], "x" = df_S_i_a$x)))
+  }else if (individual == TRUE) { # Single index
+    return(data.frame("In" = values, "x" = df_S_i_a$x))
   }
 }
