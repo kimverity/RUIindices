fc42c36217b8b4f3d2a6599a68b30393ad1e281d
vesmanojlovic <manojlovicveselin@gmail.com>
2024-01-30 18:16:23 +0000

    phylo not working, used newick

--- R/df_tree.R
+++ R/df_tree.R
@@ -1,47 +1,62 @@
-#' Reads in data frame tree (see RUtreebalance repo) and converts to phylo
-#' object
-#'
-#' The df-tree is a data frame with columns: Identity, Parent, Population, and
-#' optionally a column called Generation (from demon simulations) which is used
-#' to set the branch lengths. If Generation is not included, the branch lengths
-#' are all set to 1.
-#'
-#' @param df_tree A data frame with columns: Identity, Parent, Population, and
-#' optionally Generation.
-#' @returns A phylo object and a data frame with node abundances.
-#' @examples
-#' df_tree <- data.frame(Identity=c(0,1,2,3),
-#'                    Parent=c(0,0,1,1),
-#'                    Population=c(1,1,2,2),
-#'                    Generation=c(0,1,3,5))
-#' tree <- df_tree_to_phylo(df_tree)
-#'
+#' convert a data frame tree to igraph
+#' @param dft data frame tree
+#' @return igraph object and node attribute data frame
 #' @export
-df_tree_to_phylo <- function(df_tree) {
-    # Check that the df_tree has the correct columns
-    if (!all(c("Identity", "Parent", "Population") %in% colnames(df_tree))) {
-        stop("df_tree must have columns Identity, Parent & Population")
+dfToIgraph <- function(dft) {
+  id_map <- as.numeric(factor(dft$Identity))
+  parent_map <- match(dft$Parent, dft$Identity)
+  edge_matrix <- cbind(parent_map, id_map)
+  edge_matrix <- edge_matrix[edge_matrix[, 1] != edge_matrix[, 2], ]
+  if ("Generation" %in% names(dft) &&
+      !is.null(dft$Generation)) {
+    edge_lengths <- dft$OriginTime - dft$Generation
+  } else if ("OriginTime" %in% names(dft) &&
+             !is.null(dft$OriginTime)) {
+    edge_lengths <- dft$OriginTime[id_map] - dft$OriginTime[parent_map]
+  } else {
+    edge_lengths <- rep(1, nrow(edge_matrix))
+  }
+  g <- igraph::graph_from_edgelist(as.matrix(edge_matrix), directed = TRUE)
+  igraph::E(g)$weight <- edge_lengths
+  nadf <- data.frame(names = dft$Identity, values = dft$Population)
+  return(list(g, nadf))
+}
+
+#' convert igraph to Newick
+#' @param g igraph object
+#' @return Newick string
+#' @export
+igraphToNewick <- function(g) {
+  if (!igraph::is_tree(g)) {
+    stop("igraph object is not a tree")
+  }
+  if (!igraph::is_directed(g)) {
+    stop("igraph object is directed")
+  }
+  buildNewick <- function(node) {
+    children <- igraph::neighbors(g, node, mode = "out")
+    if (length(children) == 0) {
+      return(as.character(node))
+    } else {
+      childStrings <- sapply(children, buildNewick, USE.NAMES = FALSE)
+      return(paste0("(", paste(childStrings, collapse = ","), ")",
+                    as.character(node)))
     }
-    na_dataframe <- data.frame("Names" = as.character(df_tree$Identity),
-                               "Values" = df_tree$Population)
-    phylo_tree <- list()
-    phylo_tree$edge <- as.matrix(df_tree[, c("Parent", "Identity")])
-    phylo_tree$tip.label <- as.character(setdiff(df_tree$Identity,
-                                                 df_tree$Parent))
-    phylo_tree$Nnode <- length(unique(df$Parent)) - 1
-    edge_lengths <- numeric(nrow(df_tree))
-    for (i in 1:nrow(df_tree)) {
-        if (df_tree$Identity[i] == 0) {
-            edge_lengths[i] <- 0 # root node
-        }
-        else {
-            parent_gen <- df_tree$Generation[df_tree$Identity == df$Parent[i]]
-            edge_lengths[i] <- df_tree$Generation[i] - parent_gen
-        }
-    }
-    phylo_tree$edge.length <- edge_lengths
-    internal_node_labels <- df$internal_label[df$Identity %in% phylo_tree$edge[,1]]
-    phylo_tree$node.label <- as.character(internal_node_labels)
-    phylo_tree$node.label <- paste("Node", seq_len(phylo_tree$Nnode))
-    return(list(phylo_tree, na_dataframe))
+  }
+  root <- igraph::V(g)[igraph::degree(g, mode = "in") == 0]
+  if (length(root) != 1) {
+    stop("The tree does not have a unique root")
+  }
+  newickString <- buildNewick(root)
+  return(paste0(newickString, ";"))
+}
+
+#' convert data frame tree to Newick
+#' @param dft data frame tree
+#' @return Newick string and node attribute data frame
+#' @export
+dfToNewick <- function(dft) {
+  treelist <- dfToIgraph(dft)
+  tree <- igraphToNewick(treelist[[1]])
+  return(list(tree, treelist[[2]]))
 }
